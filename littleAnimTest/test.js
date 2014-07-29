85db732db43c51366622681e384e75facc2e063c
André Bose do Amaral <inquiries@mcnto.com>
2014-07-29 11:37:14 +0400

    Some random tests

---
+++ littleAnimTest/test.js
@@ -0,0 +1,20 @@
+	$(window).scroll(function() {
+		$('#test').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideLeft");
+			}
+		});
+
+		$('#test2').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideRight");
+			}
+		});
+
+	});
