5f864387f6bc5eba8fc6f077bc2d09e40d2b7cdd
André Bose do Amaral <andregoiano@users.noreply.github.com>
2015-05-09 19:09:02 +0800

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,8 +1,8 @@
 from twilio.rest import TwilioRestClient
  
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "ACa92624250d881722233fe9bbd4e53d86"
-auth_token  = "80e8a7bdd20cbc681fc7438006372565"
+account_sid = ""
+auth_token  = ""
 client = TwilioRestClient(account_sid, auth_token)
  
 message = client.messages.create(body="Welcome to Mecenato!",
